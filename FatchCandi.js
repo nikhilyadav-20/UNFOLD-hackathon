46ebb78befffb425447633c8644b986903988a38
Nikhil Yadav <114644433+nikhilyadav-20@users.noreply.github.com>
2024-06-02 22:56:23 +0530

    Create FatchCandi.js

---
+++ FatchCandi.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useEffect, useState} from "react";
+function FatchCandi({contract, account, provider }) {
+    const [candidates, setCandidate] = useState([]);
+    console.log("Set Comp");
+    useEffect(() => {
+        const Fatch = async () => {
+            const info = await contract.getCandidate();
+            console.log(info);
+            setCandidate(info);
+            setCandidate(info);
+        };
+        contract && Fatch();
+    }, [contract]);
+    return (
+        <div>
+            <p className="text-dark h3">Candidates</p>
+        {candidate.map((candidate) =>{
+            return (
+                <div key={Math.random()}>
+                 <div>
+                  <table>
+                   <tbody>
+                    <tr>
+                     <td className="p-2">Candidate Name {candidate.name}</td>
+
+                     <td className="p-2">
+                       {" "}
+                       Candidate Address {candidate._CandidateAddress}
+                       </td>
+
+                       <td className="p-2"> Voted {candidate.vote.tostring()}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+);
+})}
+</div>
+);
+}
+export default FatchCandi;
