506523591833f625a4c63a57a5bedd5e5aad8df9
Nikhil Yadav <114644433+nikhilyadav-20@users.noreply.github.com>
2024-06-02 22:59:05 +0530

    Create sec.js

---
+++ sec.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { useState } from "react";
+import * as ReactBootStrap from "react-bootstrap";
+function Sec({ contract, account, provide}) {
+    const [loading, setLoading] = useState(false);
+    const SetCandidate = async (e) => {
+        e.preventDefault();
+        const Address = document.getElementById("address").value;
+        const Name = document.getElementById("name").value;
+        if (Address && Name) {
+            console.log(Address);
+            await contract.SetCandidate(Address, Name);
+            console.log("Set!");
+            window.location.reload();
+            setLoading(true);    
+        }else {
+            alert("please Fill input Feild");
+        }
+    };
+    return (
+        <div className="pb-3">
+        <form onSubmit={SetCandidate}>
+        <div className="form-group p-2">
+        <label>Address</label>
+        <input
+        type="text"
+        placeholder="Address"
+        id="address"
+        disabled={!account}
+        class="form-control"
+        ></input>
+        </div>
+        <button
+        type="submit"
+        disabled={!account}
+        className="btn btn-dark mx-2 mt-2"
+        >
+        {!loading ? (
+            "Set Candidate"
+        ) : (
+            <ReactBootStrap.Spinner
+            as="span"
+            animation="grow"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+            />
+        )}
+        </button>
+        </form>
+        </div>
+        );
+}
